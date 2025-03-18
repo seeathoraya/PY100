86b65995619aa4fcd96d1580d0de74478895ba1f
Shamash Athoraya <shamash.athoraya@gmail.com>
2025-03-18 08:05:08 +0100

    First commit

---
+++ data_types/exercises.py
@@ -0,0 +1,24 @@
+# Exercise 1
+values = ['True', False, (1, 2, 3), 1.5, [1, 2, 3], 2, range(5), {1, 2, 3}, None, {'foo': 'bar'}]
+for value in values:
+    print(value, type(value))
+
+# Exercise 2
+names = (
+    'Asta',
+    'Butterscotch',
+    'Pudding',
+    'Neptune',
+    'Darwin',
+)
+print(names)
+
+# Exercise 3
+pets = {
+    'Asta': 'dog',
+    'Butterscotch': 'cat',
+    'Pudding': 'cat',
+    'Neptune': 'fish',
+    'Darwin': 'lizard',
+}
+print(pets)
